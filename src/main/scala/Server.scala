aa6a571aee3e0497afec966bc5d95747f3790aa5
Ricky Elrod <ricky@elrod.me>
2013-07-15 04:29:21 -0400

    This is entirely horrible.

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,51 +1,97 @@
 package net.tenthbit.server
 
-import akka.actor.{ Actor, ActorRef, ActorSystem, Props }
-import akka.io.{ IO, Tcp }
-import akka.util.ByteString
+import akka.actor.{ Actor, ActorRef, ActorLogging, ActorSystem, Deploy, Props }
+import akka.io.{ BackpressureBuffer, DelimiterFraming, IO, StringByteStringAdapter, Tcp, TcpPipelineHandler, TcpReadWriteAdapter, SslTlsSupport }
+import akka.io.TcpPipelineHandler.{ Init, WithinActorContext }
+import akka.pattern.ask
+import akka.util.{ ByteString, Timeout }
 
 import net.liftweb.json._
 import net.liftweb.json.JsonDSL._
+import net.liftweb.json.Serialization.{ read, write }
 import net.tenthbit.protocol._
 
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import java.io.FileInputStream
 import java.net.InetSocketAddress
+import java.security.KeyStore
+
+import javax.net.ssl.{ SSLContext, SSLEngine, TrustManagerFactory, KeyManagerFactory }
+
+object TenthbitSSL {
+  // https://github.com/cloudaloe/pipe/blob/master/src/main/scala/pipe/ssl.scala
+  val context: SSLContext = SSLContext.getInstance("SSLv3")
+  val keyStore = KeyStore.getInstance("JKS")
+  val keyStoreFile = new FileInputStream("/home/ricky/devel/tenthbit/ssl/keystore.jks")
+  val keyStorePassword = ""
+  keyStore.load(keyStoreFile, keyStorePassword.toCharArray)
+  keyStoreFile.close
+
+  val keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm)
+  val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
+  keyManagerFactory.init(keyStore, keyStorePassword.toCharArray)
+  trustManagerFactory.init(keyStore)
+  context.init(keyManagerFactory.getKeyManagers, trustManagerFactory.getTrustManagers, null)
+
+  def getEngine: SSLEngine = {
+    val engine: SSLEngine = context.createSSLEngine
+    engine setUseClientMode false
+    engine setNeedClientAuth false
+    engine
+  }
+}
 
 case class Broadcast(obj: Payload)
 case class BroadcastString(obj: String)
 case class Disconnection(handler: ActorRef)
 
-class Server extends Actor {
+class Server extends Actor with ActorLogging {
   import Tcp._
   import context.system
 
-  val clientHandlers = scala.collection.mutable.ArrayBuffer[ActorRef]()
+  implicit val defaults = DefaultFormats
+  implicit val timeout = Timeout(300.millis)
+  val clientPipelines = scala.collection.mutable.ArrayBuffer[(Init[WithinActorContext, String, String], ActorRef)]()
 
-  IO(Tcp) ! Bind(self, new InetSocketAddress("localhost", 10817))
+  IO(Tcp) ! Bind(self, new InetSocketAddress("0.0.0.0", 10817))
 
-  def receive = {
-    case Broadcast(obj) => clientHandlers.foreach(_ ! obj)
-    case BroadcastString(obj) => clientHandlers.foreach(_ ! obj)
-    case Disconnection(handler) => clientHandlers -= handler
-    case b @ Bound(localAddress) => println("Hello: " + localAddress)
-    case CommandFailed(_: Bind) => context stop self
-    case c @ Connected(remote, local) => {
-      val handler = context.actorOf(Props(classOf[SimplisticHandler], self, sender))
-      clientHandlers += handler
+  def receive: Receive = {
+    case _: Bound => context.become(bound(sender))
+  }
+
+  def bound(listener: ActorRef): Receive = {
+    case Broadcast(obj) => {
+      clientPipelines.foreach(e => println(e))
+      clientPipelines.foreach { case (init, pipeline) => pipeline ! init.Command(write(obj) + "\n") }
+    }
+    case Connected(remote, _) => {
+      val init = TcpPipelineHandler.withLogger(
+        log,
+        new StringByteStringAdapter("utf-8") >>
+          new DelimiterFraming(maxSize = 1024, delimiter = ByteString('\n'), includeDelimiter = true) >>
+            new TcpReadWriteAdapter >>
+              new SslTlsSupport(TenthbitSSL.getEngine) >>
+                new BackpressureBuffer(lowBytes = 100, highBytes = 1000, maxBytes = 1000000))
 
       val connection = sender
-      connection ! Register(handler)
+      val handler = context.actorOf(Props(new SslHandler(self, init)).withDeploy(Deploy.local))
+      val pipeline = context.actorOf(TcpPipelineHandler.props(init, connection, handler).withDeploy(Deploy.local))
+      clientPipelines += ((init, pipeline))
 
-      val welcome = Welcome(
-        Some(
-          WelcomeExtras(
-            "elrod.me",
-            "scala-10b/1.0.0",
-            List("password", "anonymous"),
-            System.currentTimeMillis)))
+      (connection ? Tcp.Register(pipeline)) onComplete { case _ =>
+        val welcome = Welcome(
+          Some(
+            WelcomeExtras(
+              "elrod.me",
+              "scala-10b/1.0.0",
+              List("password", "anonymous"),
+              System.currentTimeMillis)))
 
-      handler ! welcome
+        pipeline ! init.Command(write(welcome) + "\n")
+      }
     }
-
   }
 }
 
--- src/main/scala/SimplisticHandler.scala
+++ src/main/scala/SimplisticHandler.scala
@@ -1,7 +1,8 @@
 package net.tenthbit.server
 
-import akka.actor.{ Actor, ActorRef, Props }
+import akka.actor.{ Actor, ActorLogging, ActorRef, Props }
 import akka.io.{ IO, Tcp }
+import akka.io.TcpPipelineHandler.{ Init, WithinActorContext }
 import akka.util.ByteString
 
 import net.liftweb.json._
@@ -9,42 +10,23 @@ import net.liftweb.json.JsonDSL._
 import net.liftweb.json.Serialization.{ read, write }
 import net.tenthbit.protocol._
 
-class SimplisticHandler(server: ActorRef, connection: ActorRef) extends Actor {
-  import Tcp._
-  implicit val formats = Serialization.formats(NoTypeHints)
 
-  def receive = {
-    case payload: Payload => connection ! Write(ByteString(write(payload) + "\n", "utf-8"))
-    case payload: String => connection ! Write(ByteString(write(payload) + "\n", "utf-8"))
-    case e: ConnectionClosed => server ! Disconnection(self)
-    case Received(data) => {
-      val json = parse(data.decodeString("utf-8"))
-      (json \ "op").extract[String] match {
-        case "act" => server ! BroadcastString(write(json))
-        case "auth" => {
-          val username = (json \ "ex" \ "username") match {
-            case JString(username) => Some(username)
-            case _ => None
-          }
-          val password = (json \ "ex" \ "password") match {
-            case JString(password) => Some(password)
-            case _ => None
-          }
-          if (username == Some("relrod") && password == Some("mypassword")) {
-            self ! (
-              ("op" -> "ack") ~
-              ("ex" ->
-                ("for" -> "auth")))
-          } else {
-            self ! (
-              ("op" -> "error") ~
-              ("ex" ->
-                ("for" -> "auth")))
-          }
+class SslHandler(server: ActorRef, init: Init[WithinActorContext, String, String])
+  extends Actor with ActorLogging {
+
+    import Tcp._
+
+    implicit val defaults = DefaultFormats
+
+    def receive = {
+      // TODO: Actually do something with this.
+      case e: ConnectionClosed => server ! Disconnection(self)
+      case init.Event(data) => {
+        val json = parse(data)
+        (json \ "op").extract[String] match {
+          case "act" => server ! BroadcastString(write(json))
+          case otherwise => server ! Broadcast(Ack(Some(AckExtras(otherwise))))
         }
-        case otherwise => server ! Broadcast(Ack(Some(AckExtras(otherwise))))
       }
     }
-    case PeerClosed => context stop self
-  }
 }
