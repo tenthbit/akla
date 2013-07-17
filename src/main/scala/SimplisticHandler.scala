a4fa548fe6d7f4569c0ad22bb932e4a5f4195268
Ricky Elrod <ricky@elrod.me>
2013-07-17 12:20:04 -0400

    Handle adding acks to packets.

--- src/main/scala/SimplisticHandler.scala
+++ src/main/scala/SimplisticHandler.scala
@@ -6,6 +6,7 @@ import akka.io.TcpPipelineHandler.{ Init, WithinActorContext }
 import akka.util.ByteString
 
 import net.liftweb.json._
+import net.liftweb.json.Extraction.decompose
 import net.liftweb.json.JsonDSL._
 import net.liftweb.json.Serialization.{ read, write }
 import net.tenthbit.protocol._
@@ -18,14 +19,19 @@ class SslHandler(server: ActorRef, init: Init[WithinActorContext, String, String
 
     implicit val defaults = DefaultFormats
 
+    private def renderJValue(a: JValue): init.Command =
+      init.Command(write(a) + "\n")
+
+    private def addAck(a: JValue): JValue =
+      a merge decompose(Map("ex" -> decompose(Map("isack" -> true))))
+
     def receive = {
       // TODO: Actually do something with this.
       case e: ConnectionClosed => server ! Disconnection(self)
       case init.Event(data) => {
         val json = parse(data)
         (json \ "op").extract[String] match {
-          case "act" => server ! BroadcastString(write(json))
-          case otherwise => server ! Broadcast(Ack(Some(AckExtras(otherwise))))
+          case otherwise => sender ! renderJValue(addAck(json))
         }
       }
     }
